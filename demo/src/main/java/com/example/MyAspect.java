f9e9413e99dcab602dfad7849621b6055bba8b11
Jinya Namiki <83228978+avenue68@users.noreply.github.com>
2021-12-20 17:23:32 +0900

    Change to @Around from @AfterReturning to reproduce problem.

--- demo/src/main/java/com/example/MyAspect.java
+++ demo/src/main/java/com/example/MyAspect.java
@@ -1,17 +1,21 @@
 package com.example;
 
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 
 @Aspect
 public class MyAspect {
 
-	@AfterReturning("""
+	@Around("""
 			@within(com.example.MyAnnotation)
 			&& (execution(* *.*(..)) || execution(* .new(..)))
 			""")
-	public void intercept(JoinPoint JoinPoint) throws Throwable {
+	public Object intercept(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
 		System.out.println("********** InvocationAspect intercepted.");
+		var returnValue = proceedingJoinPoint.proceed();
+		System.out.println("********** InvocationAspect finished.");
+
+		return returnValue;
 	}
 }
